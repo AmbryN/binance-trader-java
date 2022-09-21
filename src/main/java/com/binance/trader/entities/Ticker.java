0312f04742805b7df1c04e12ae1b209212e2cee9
AmbryN <nicolas.ambry@gmail.com>
2022-09-21 14:20:24 +0200

    Added test for Ticker

--- src/main/java/com/binance/trader/entities/Ticker.java
+++ src/main/java/com/binance/trader/entities/Ticker.java
@@ -2,9 +2,14 @@ package com.binance.trader.entities;
 
 public class Ticker {
     String symbol;
-    Float price;
+    double price;
 
-    public Float getPrice() {
+    public Ticker(String symbol, double price) {
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public double getPrice() {
         return this.price;
     }
 }
