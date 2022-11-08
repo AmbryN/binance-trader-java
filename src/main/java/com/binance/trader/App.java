1829030fd79497d000de97485142e34f411c0cdc
AmbryN <nicolas.ambry@gmail.com>
2022-11-08 09:18:03 +0100

    Deleted unused imports and functions

--- src/main/java/com/binance/trader/App.java
+++ src/main/java/com/binance/trader/App.java
@@ -4,21 +4,12 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import com.binance.trader.utils.Logging;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
-import java.util.zip.Deflater;
-
 public class App
 {
     public static void main( String[] args ) throws InterruptedException {
         Logger logger = Logging.getInstance();
         logger.setLevel(Level.WARN);
         Trader trader = new Trader();
-//        FutureTask task = new FutureTask<>(trader, null);
-//        ExecutorService executor = Executors.newFixedThreadPool(1);
-//
-//        executor.execute(task);
         trader.run();
     }
 }
